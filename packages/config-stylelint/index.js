35eed17bd7f3a3ec90ef2df9a6f59c820c6817c1
Anna <annaing.dev@gmail.com>
2026-09-18 17:16:29 +0900

    chore: eslint/typescript/stylelint 공유 설정 패키지 추가

---
+++ packages/config-stylelint/index.js
@@ -0,0 +1,8 @@
+export default {
+  extends: ['stylelint-config-standard', 'stylelint-config-css-modules'],
+  rules: {
+    'selector-class-pattern': null,
+    'custom-property-pattern': null,
+    'value-keyword-case': null,
+  },
+};
